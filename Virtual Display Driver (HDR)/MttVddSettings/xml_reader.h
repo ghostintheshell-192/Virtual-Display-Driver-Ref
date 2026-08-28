06a38633879daaebca06512443821e77e54d2d87
Valentina Malavenda <valentina.malavenda@txtgroup.com>
2026-08-28 11:57:31 +0200

    simplified logic for xml reading.

--- Virtual Display Driver (HDR)/MttVddSettings/xml_reader.h
+++ Virtual Display Driver (HDR)/MttVddSettings/xml_reader.h
@@ -29,51 +29,18 @@ class XmlReader
 		}
 		std::string raw_value;
 
-		if (values.size() == 3)
+		tinyxml2::XMLElement *current = settings_file.RootElement();
+		for (const auto &segment : values)
 		{
-			auto elem = element->FirstChildElement(values[0].c_str());
-			if (!elem)
+			current = current->FirstChildElement(segment.c_str());
+			if (!current)
 			{
-				std::cout << "Node not found in xml: " << values[0].c_str();
+				std::cout << "Node not found in xml: " << segment.c_str();
 				return false;
 			}
-			auto sub_elem = elem->FirstChildElement(values[1].c_str());
-			if (!sub_elem)
-			{
-				std::cout << "Node not found in xml: " << values[1].c_str();
-				return false;
-			}
-			auto setting = sub_elem->FirstChildElement(values[2].c_str());
-			if (!setting)
-			{
-				std::cout << "Node not found in xml: " << values[2].c_str();
-				return false;
-			}
-			raw_value = setting->GetText();
 
-			// raw_value = element->FirstChildElement(values[0].c_str())
-			//				->FirstChildElement(values[1].c_str())
-			//				->FirstChildElement(values[2].c_str())
-			//				->GetText();
 		}
-		else if (values.size() == 2)
-		{
-			auto elem = element->FirstChildElement(values[0].c_str());
-			if (!elem)
-			{
-				std::cout << "Node not found in xml: " << values[0].c_str();
-				return false;
-			}
-			auto setting = elem->FirstChildElement(values[1].c_str());
-			if (!setting)
-			{
-				std::cout << "Node not found in xml: " << values[1].c_str();
-				return false;
-			}
-			raw_value = setting->GetText();
-
-			//raw_value = element->FirstChildElement(values[0].c_str())->FirstChildElement(values[1].c_str())->GetText();
-		}
+		raw_value = current->GetText();
 
 		if (raw_value.empty())
 			return false;
