4dcfefa372980f1f6b8eb6b96dcee75743857f24
Valentina Malavenda <valentina.malavenda@txtgroup.com>
2026-09-08 15:32:34 +0200

    first draft for logging class

---
+++ Virtual Display Driver (HDR)/MttVddRefactor/logger.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <chrono>
+#include <string>
+#include <windows.h>
+
+namespace Refactoring
+{
+
+enum class LogType : __int8
+{
+	None		= 0,
+	Error		= 1,
+	Info		= 2,
+	Pipe		= 3,
+	Debug		= 4,
+	Warning		= 5,
+	Testing		= 6,
+	Companion	= 7,
+};
+
+class Logger
+{
+  public:
+	Logger(std::string base_dir, bool enable_std_logs, bool enable_debug_logs, bool send_logs_through_pipe);
+	~Logger();
+
+	void SendToPipe(const std::string &logMessage);
+
+	void Init(HANDLE *ext_pipe);
+
+	void Message(LogType type, std::string msg);
+
+	void ToggleStandardLogs(bool enable);
+	void ToggleDebugLogs(bool enable);
+	void TogglePipedLogs(bool enable);
+
+  protected:
+  private:
+	void OpenLogFile();
+	void CloseLogFile();
+	void ChangeDate();
+
+	bool HasDateChanged() const;
+
+	std::chrono::year_month_day today;
+	std::string base_logpath;
+
+	bool standard_logs;
+	bool debug_logs;
+	bool piped_logs;
+
+	FILE *m_log_file;
+	HANDLE *m_pipe_handle;
+};
+} // namespace Refactoring
